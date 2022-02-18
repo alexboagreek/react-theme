07d4492dc363bdaa188d2d137e6ddece8979b6e9
Alex <olivesargreek@gmail.com>
2022-02-18 10:15:22 +0300

    create: TextContentProvider

--- react-theme/src/components/Context/ThemeContext.jsx
+++ react-theme/src/components/Context/ThemeContext.jsx
@@ -1,4 +1,15 @@
 import React, { createContext, useState } from "react";
 
-
 export const ThemeContext = createContext();
+
+const ThemeContextProvider = (props) => {
+    const [theme, setTheme] = useState('Hello React Theme!');
+
+    return (
+        <ThemeContext.Provider value={{ theme }}>
+            { props.children }
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeContextProvider;
