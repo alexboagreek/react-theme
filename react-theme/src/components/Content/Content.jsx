2ca1595d35e3fabdba98c504a38d49f7f0fe2774
Alex <olivesargreek@gmail.com>
2022-02-18 10:10:14 +0300

    init: init project && basic structure

---
+++ react-theme/src/components/Content/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Content.css';
+
+function Content() {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet.</h1>
+        </div>
+    )
+}
+
+export default Content;
